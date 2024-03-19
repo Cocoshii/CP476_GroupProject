0fd06b6cf6d1945379eaa158e64816bcd62fb8a2
Cocoshii <gracelinabunny@gmail.com>
2024-03-19 16:09:31 -0400

    Added connect_database.php file. Should connect successfully to database on local host, if you have created cp476_db on your own computer. Please refer to setup.txt for help on how to connect the code to the MySQL database.

---
+++ connect_database.php
@@ -0,0 +1,26 @@
+<?php
+
+// CODE FROM LECTURE SLIDE: WEEK 5 PART 2
+$dbName = "cp476_db";
+$dsn = "mysql:host=localhost;dbname=cp476_db;charset=utf8mb4"; // CP476_DB is the name of the database to connect to
+$options = [
+PDO::ATTR_EMULATE_PREPARES => false, // turn off emulation mode
+PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
+PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
+];
+try {
+    $password = "insert your password here"; // REPLACE WITH YOUR MYSQL PASSWORD BEFORE RUNNING FILE
+    // Note: When editing this and committing changes, it's best to update $password back to a placeholder string
+    // if you don't want others to see your password here.
+
+    $conn = new PDO($dsn, "root", $password, $options);
+} catch (PDOException $e) {
+    error_log($e->getMessage());
+    exit('Something wrong');
+}
+
+echo "Connected successfully to database: " . $dbName . "\n";
+
+$conn=null; // close connection
+
+?>
