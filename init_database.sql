e66e918d08d6cd6ebafd9522526b00ae757718ff
cscoding59 <117619211+cscoding59@users.noreply.github.com>
2024-03-25 15:21:31 -0400

    Added cascade constraints

--- init_database.sql
+++ init_database.sql
@@ -18,7 +18,7 @@ Test1 FLOAT NOT NULL DEFAULT 0.0 CHECK (Test1>=0 AND Test1 <= 100),
 Test2 FLOAT NOT NULL DEFAULT 0.0 CHECK (Test2>=0 AND Test2 <= 100), 
 Test3 FLOAT NOT NULL DEFAULT 0.0 CHECK (Test3>=0 AND Test3 <= 100),
 FinalExam FLOAT NOT NULL DEFAULT 0.0 CHECK (FinalExam>=0 AND FinalExam <= 100),
-FOREIGN KEY(StudentID) REFERENCES NameTable(StudentID)); -- relational db tables linked by primary key StudentID from NameTable
+FOREIGN KEY(StudentID) REFERENCES NameTable(StudentID) ON UPDATE CASCADE ON DELETE CASCADE); -- relational db tables linked by primary key StudentID from NameTable
 
 -- Creates the "Final Grades Table"
 CREATE TABLE IF NOT EXISTS FinalGrades (
@@ -26,6 +26,6 @@ CREATE TABLE IF NOT EXISTS FinalGrades (
     StudentName VARCHAR(100) NOT NULL,
     CourseCode VARCHAR(5) NOT NULL,
     FinalGrade FLOAT NOT NULL,
-    FOREIGN KEY(StudentID) REFERENCES NameTable(StudentID),
+    FOREIGN KEY(StudentID) REFERENCES NameTable(StudentID) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY(StudentID, CourseCode)
 );
